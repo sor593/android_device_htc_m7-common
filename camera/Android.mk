c96607073bc27457bbbb71354824bd12c37b0c7e
Simon Shields <simon@lineageos.org>
2018-08-13 20:16:07 +0500

    m7-common: CameraWrapper: Add missing libs for headers
    Change-Id: I245d247d21585990f37f7c45f31762a5db9543a3
    
    # Conflicts:
    #	camera/Android.mk

--- camera/Android.mk
+++ camera/Android.mk
@@ -28,15 +28,11 @@ LOCAL_C_INCLUDES := \
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
+LOCAL_STATIC_LIBRARIES := libbase libarect
 LOCAL_SHARED_LIBRARIES := \
-    libhardware \
-    liblog \
-    libcamera_client \
-    libgui \
-    libhidltransport \
-    libsensor \
-    libutils \
-    android.hidl.token@1.0-utils
+    libhardware liblog libcamera_client libutils libcutils libdl \
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
 
 LOCAL_STATIC_LIBRARIES := \
     libarect
